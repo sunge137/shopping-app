bf267701b6ef2c71dfa0c49dd4c1d3019fee8bc6
Eric Sung <sunge137@gmail.com>
2026-06-12 14:31:10 -0500

    Changed docUrl to use env

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,5 @@
 function ResumePage() {
-  const docUrl = "https://docs.google.com/document/d/1blgR1nSuxjvFcJOuxEG-qWfkz2mytMxP/edit?usp=sharing&ouid=114420375710480008433&rtpof=true&sd=true";
+  const docUrl = process.env.GOOGLE_DOC_URL;
   return (
     <div className="doc-container" style={{ width: "100%", height: "800px" }}>
       <a className="text-blue-500 hover:text-blue-700" href={docUrl} target="_blank" rel="noopener noreferrer">
